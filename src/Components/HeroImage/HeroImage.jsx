1dac682b643e67be17660902c0d3daf6c71db37c
ohziiilov3 <klbaskerville0520@gmail.com>
2023-12-23 00:15:53 -0800

    feat: add HeroImage component with styling

--- src/Components/HeroImage/HeroImage.jsx
+++ src/Components/HeroImage/HeroImage.jsx
@@ -1,9 +1,15 @@
-
+import "../HeroImage/HeroImageStyles.css"
+import heroImg from "../../assets/images/LandingPage-Pic.png"; 
 
 const HeroImage = () => {
   return (
-    <div>HeroImage</div>
-  )
+    <div className="heroImage-container">
+      <figure>
+        <img className="heroImage" src={heroImg} alt="Hero Image" />
+      </figure>
+      <div className="ellipse"> </div>
+    </div>
+  );
 }
 
-export default HeroImage
+export default HeroImage;
